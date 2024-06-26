07e4b5d6861b2ae65a190164ae13d848b7dd946a
JamieDeMaria <jamie@dagsterlabs.com>
2024-06-26 11:38:19 -0400

    first pass DA submits backfills

--- python_modules/dagster/dagster/_core/instance/__init__.py
+++ python_modules/dagster/dagster/_core/instance/__init__.py
@@ -3231,3 +3231,6 @@ class DagsterInstance(DynamicPartitionsStore):
 
     def backfill_log_storage_enabled(self) -> bool:
         return False
+
+    def da_emit_backfills(self) -> bool:
+        return True  # TODO: make False before merge
